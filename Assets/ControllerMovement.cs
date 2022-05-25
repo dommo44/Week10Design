69ccfd65ace2a3b15e233a09a57e22d6dfe0820a
dommo44 <60171090+dommo44@users.noreply.github.com>
2022-05-25 15:04:00 +1000

    Final Submission

---
+++ Assets/ControllerMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovement : MonoBehaviour
+{
+    private bool gripPressed = false;
+    private Vector2 movement = new Vector2(0, 0);
+    private Transform xrRig;
+    private Transform mainCamera;
+    private float deadzone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
